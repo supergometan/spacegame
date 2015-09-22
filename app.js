7836b05cc11a6de6050109ff3a25012e3d45e105
supergometan <max-g@gmx.net>
2015-09-22 15:19:44 +0200

    port changed to 8080

--- app.js
+++ app.js
@@ -97,8 +97,8 @@ function animate() {
 
 animate();
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
 
 // view engine setup
